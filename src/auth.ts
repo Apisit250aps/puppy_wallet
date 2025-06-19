d3e8cd32f5c920b63d59abb9c28180f56e2428d9
YUapisit <aps.apisit250@gmail.com>
2025-06-19 22:06:37 +0700

    Add authentication features and UI components; update dependencies and styles

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,10 @@
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import NextAuth from 'next-auth'
-import client from './lib/db';
-import authConfig from './auth.config';
+import client from './lib/db'
+import authConfig from './auth.config'
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: MongoDBAdapter(client),
-  ...authConfig
+  session: { strategy: 'jwt' },
+  ...authConfig,
 })
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthRequest } from 'next-auth'
 import authConfig from './auth.config'
 import { NextResponse } from 'next/server'
 
 const { auth } = NextAuth(authConfig)
-export default auth(async function middleware() {
+export default auth(async (req: NextAuthRequest) => {
   // Your custom middleware logic goes here
+  console.log(req.auth)
   return NextResponse.next()
 })
 
